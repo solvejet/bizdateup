ec5b43c321cf6f0d6a9e4c81a0088ffa58b93604
karansxa <karansxa@gmail.com>
2024-12-01 23:01:37 +0530

    Typescript Fixed 3

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,6 @@
 // src/lib/utils.ts
-import type { ClassValue } from './types'
-import { twMerge } from 'tailwind-merge'
+import { type ClassNameValue, twMerge } from "tailwind-merge"
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(inputs)
+export function cn(...inputs: ClassNameValue[]) {
+  return twMerge(...inputs)
 }
